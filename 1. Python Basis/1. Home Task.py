b5e30734435c410b16933db234c0eeca9f732c15
bartczukjakub <bartczukjakub@gmail.com>
2025-07-18 11:40:17 +0200

    Adding small changes for Vlad to review

--- 1. Python Basis/1. Home Task.py
+++ 1. Python Basis/1. Home Task.py
@@ -10,14 +10,7 @@ import random
 
 # 1. Create list of 100 random numbers from 0 to 1000
 
-# Create empty list
-
-number_list = []
-
-# Generate random 100 numbers fron range of 0-1000 and append them to number_list
-
-for num in range(100):
-    number_list.append(random.randint(0,1000))
+number_list = [random.randint(0,1000) for _ in range(100)]
 
 # 2. Sort list from min to max (without using sort())
 
@@ -25,13 +18,17 @@ for num in range(100):
 
 sorted_list = []
 
+# Copying original list to preserve it
+
+unsorted_copy = list(number_list)
+
 #  Do the loop X times, equal to length of original list, and with each loop append minimum value from original list to the sorted list.
 #  In each loop this minimum value is being removed from original list so the next loop selects another min value.
 
-for num in range(len(number_list)):
-    num_to_append = min(number_list)
+for num in range(len(unsorted_copy)):
+    num_to_append = min(unsorted_copy)
     sorted_list.append(num_to_append)
-    number_list.remove(num_to_append)
+    unsorted_copy.remove(num_to_append)
 
 # 3. Calculate average for even and odd numbers
 
@@ -42,9 +39,10 @@ odd_list = [number for number in sorted_list if number not in even_list]
 
 # Calculate averages for numbers in even and odd lists
 
-even_number_average = round(sum(even_list) / len(even_list), 2)
-odd_list_average = round(sum(odd_list) / len(odd_list), 2)
+even_number_average = sum(even_list) / len(even_list)
+odd_list_average = sum(odd_list) / len(odd_list)
 
 # 4. Print both average results in console
 
-print(even_number_average, odd_list_average)
+print(f'Average for even numbers is {even_number_average:.2f}.\nAverage for odd numbers is {odd_list_average:.2f}.')
+
